ae255f1aa24d526e74a4d9d8fcd899a214e01053
novohispano@gmail.com <novohispano@gmail.com>
2013-04-11 10:15:39 -0600

    Working on creating a user with random password

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,11 +16,11 @@ class OrdersController < ApplicationController
 
   def create
     @order = Order.create_and_charge(cart: current_cart,
-                                     user: current_user,
+                                     user: user_for_order,
                                      token: params[:stripeToken])
     if @order.valid?
       session[:cart] = current_cart.destroy
-      Mailer.order_confirmation(current_user, @order).deliver
+      Mailer.order_confirmation(user_for_order, @order).deliver
       redirect_to account_order_path(@order),
         :notice => "Order submitted!"
     else
@@ -30,15 +30,29 @@ class OrdersController < ApplicationController
 
   def buy_now
     @order = Order.create_and_charge(cart: Cart.new({params[:order] => '1'}),
-                                     user: current_user,
+                                     user: user_for_order,
                                      token: params[:stripeToken])
     if @order.valid?
       session[:cart] = current_cart.destroy
-      Mailer.order_confirmation(current_user, @order).deliver
+      Mailer.order_confirmation(user_for_order, @order).deliver
       redirect_to account_order_path(@order),
         :notice => "Order submitted!"
     else
       redirect_to :back, :notice => "Checkout failed."
     end
   end
+
+  private
+
+  def user_for_order
+    # is there a current_user?
+    #   is there a user by email address?
+    #     generate a user by email adddress, assigning a random password
+    user = current_user
+    user ||= User.find_by_email(email_adddress)
+    user ||= User.create_public_user
+    user
+  end
+
+  
 end
