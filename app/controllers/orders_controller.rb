14ecae2ff5dc35b317101f5f9fbeb8311c40adf2
novohispano@gmail.com <novohispano@gmail.com>
2013-04-13 18:22:37 -0600

    Reworking on the form after stash

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,40 +1,65 @@
 class OrdersController < ApplicationController
-  before_filter :require_login
+  # before_filter :require_login
 
   def index
     @orders = current_user.orders
   end
 
   def show
-    @order = current_user.orders.find_by_id(params[:id])
-    @order_items = @order.order_items
+    order = Order.find(params[:id])
+    current_user = User.find(session[:user_id])
+
+    if current_user.id == order.user_id
+      @order = Order.find(params[:id])
+    else
+      redirect_to account_orders_path
+    end
   end
 
   def create
+    session[:user_id] = user_for_order.id
+
     @order = Order.create_and_charge(cart: current_cart,
-                                     user: current_user,
+                                     user: user_for_order,
                                      token: params[:stripeToken])
     if @order.valid?
       session[:cart] = current_cart.destroy
-      Mailer.order_confirmation(current_user, @order).deliver
-      redirect_to account_order_path(@order),
+
+      shipping_params = params[:shipping].merge(user_id: user_for_order.id, order_id: @order.id)
+      shipping = Shipping.create(shipping_params)
+
+      billing_params = params[:billing].merge(user_id: user_for_order.id, order_id: @order.id)
+      billing = Billing.create(billing_params)
+
+      Mailer.order_confirmation(user_for_order, @order).deliver
+      redirect_to order_path(@order),
         :notice => "Order submitted!"
     else
-      redirect_to store_cart_path(current_store), :notice => "Checkout failed."
+      redirect_to cart_path, :notice => "Checkout failed."
     end
   end
 
   def buy_now
     @order = Order.create_and_charge(cart: Cart.new({params[:order] => '1'}),
-                                     user: current_user,
+                                     user: user_for_order(params[:order][:email]),
                                      token: params[:stripeToken])
     if @order.valid?
       session[:cart] = current_cart.destroy
-      Mailer.order_confirmation(current_user, @order).deliver
+      Mailer.order_confirmation(user_for_order, @order).deliver
       redirect_to account_order_path(@order),
         :notice => "Order submitted!"
     else
       redirect_to :back, :notice => "Checkout failed."
     end
+  end
+
+  private
+
+  def user_for_order
+    user = current_user
+    email_address = params[:user][:email]
+    user ||= User.find_by_email(email_address)
+    user ||= User.create_public_user(params[:user])
+    user
   end
 end
